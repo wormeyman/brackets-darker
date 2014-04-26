dbed48f0fe1fdf4de4e622e875a021bc6ca3dcc8
Amit Mendapara <mendapara.amit@gmail.com>
2014-04-26 23:10:19 +0530

    Refactoring.

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
 define(function (require, exports, module) {
 
     var AppInit = brackets.getModule("utils/AppInit"),
+        CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror"),
         EditorManager = brackets.getModule("editor/EditorManager"),
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
         PreferencesManager = brackets.getModule("preferences/PreferencesManager");
 
-    var THEME_NAME = "base16-dark";
-    var THEME_CLASS = "cm-s-" + THEME_NAME;
-
-    function applyDark() {
-        var promise;
-        promise = ExtensionUtils.loadStyleSheet(module, "darker.css");
-        promise.then(function () {
-            EditorManager.resizeEditor();
-        });
-        return promise;
-    }
+    var DEFAULT_THEME = "base16-default-dark";
     
-    function applyTheme() {
-        var promise;
-        if (CodeMirror.defaults.theme && CodeMirror.defaults.theme !== "default") {
-            return null;
-        }        
-        promise = ExtensionUtils.loadStyleSheet(module, THEME_NAME + ".css");
-        promise.then(function () {
-            $("#editor-holder .CodeMirror").addClass(THEME_CLASS);
-            CodeMirror.defaults.theme = THEME_NAME;
-        });
-        return promise;
+    function getSettings() {
+        return {
+            theme: DEFAULT_THEME
+        };
     }
 
     AppInit.appReady(function () {
-        applyDark().then(applyTheme);
+
+        var editor = $("#editor-holder .CodeMirror"),
+            promise = ExtensionUtils.loadStyleSheet(module, "darker.css"),
+            settings = getSettings();
+
+        promise.then(function () {
+            EditorManager.resizeEditor();
+        });
+
+        if (CodeMirror.defaults.theme && CodeMirror.defaults.theme !== "default") {
+            return null;
+        }
+
+        promise = promise.then(function () {
+            return ExtensionUtils.loadStyleSheet(module, "themes/" + settings.theme + ".css");
+        });
+
+        promise.then(function () {
+            editor.addClass("cm-s-" + settings.theme);
+            CodeMirror.defaults.theme = settings.theme;
+        });
+
+        return promise;
     });
 });
